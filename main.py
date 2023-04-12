4c0800427bffc64fe92f5e33a610300ae455a4d3
Matheus Marsal <62273176+MatheusMarsal@users.noreply.github.com>
2023-04-12 17:06:51 -0300

    Click and drag rect for set a new velocity
    + Bug fix -> Border Collision better

--- main.py
+++ main.py
@@ -20,42 +20,91 @@ class App(pg.window.Window):
                             
         self.velRand()
 
-        pg.clock.schedule_interval(self.update, 1/120)
+        pg.clock.schedule_interval(self.update, 1 / 120)
 
     def update(self, dt):
-        self._rect.x += self._velRand_x * dt
-        self._rect.y += self._velRand_y * dt
+        self._rect.x += self._vel_x * dt
+        self._rect.y += self._vel_y * dt
 
-        if self._rect.x <= 0 or self._rect.x >= self.width - self._rect.width:
-            self._velRand_x = -self._velRand_x
+        if self._rect.x <= 0:
+            self._rect.x = 0
+            self._vel_x = -self._vel_x
 
-        if self._rect.y <= 0 or self._rect.y >= self.height - self._rect.height:
-            self._velRand_y = -self._velRand_y
+        if self._rect.x >= self.width - self._rect.width:
+            self._rect.x = self.width - self._rect.width
+            self._vel_x = -self._vel_x
+
+        if self._rect.y <= 0:
+            self._rect.y = 0
+            self._vel_y = -self._vel_y
+        
+        if self._rect.y >= self.height - self._rect.height:
+            self._rect.y = self.height - self._rect.height
+            self._vel_y = -self._vel_y
 
     def velRand(self):
-        self._velRand_x = randint(-self.width//3, self.width//3)
-        self._velRand_y = randint(-self.width//3, self.height//3)
+        self._vel_x = randint(-self.width // 2, self.width // 2)
+        self._vel_y = randint(-self.height // 2, self.height // 2)
 
-        if self._velRand_x == 0 or self._velRand_y == 0:
+        if self._vel_x == 0 or self._vel_y == 0:
             self.velRand()
 
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == pg.window.mouse.LEFT:
+            if x > self._rect.x and x < self._rect.x + self._rect.width and y > self._rect.y and y < self._rect.y + self._rect.height:
+                self._vel_x = 0
+                self._vel_y = 0
+
+                self._click_x = x
+                self._click_y = y
+
+                self._dx_rect_click = self._rect.x - x
+                self._dy_rect_click = self._rect.y - y
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        if button == pg.window.mouse.LEFT:
+            try:
+                if self._dx_rect_click or  self._dy_rect_click:
+                    self._vel_x = (self._click_x - x) * 2
+                    self._vel_y = (self._click_y - y) * 2
+
+                    self._dx_rect_click = 0
+                    self._dy_rect_click = 0
+
+            except:
+                pass
+
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        if buttons & pg.window.mouse.LEFT:
+            try:
+                if self._dx_rect_click or self._dy_rect_click:
+                    self._rect.x = x + self._dx_rect_click
+                    self._rect.y = y + self._dy_rect_click
+
+            except:
+                pass
+
     def on_key_press(self, symbol, modifiers):
-        if symbol in [65361, 65362, 65363, 65364]:
+        if symbol in [pg.window.key.LEFT, pg.window.key.RIGHT, pg.window.key.DOWN, pg.window.key.UP]:
             if symbol == pg.window.key.LEFT:
-                if self._velRand_x > 0:
-                    self._velRand_x = -self._velRand_x
+                if self._vel_x > 0:
+                    self._vel_x = -self._vel_x
 
             if symbol == pg.window.key.UP:
-                if self._velRand_y < 0:
-                    self._velRand_y = -self._velRand_y
+                if self._vel_y < 0:
+                    self._vel_y = -self._vel_y
 
             if symbol == pg.window.key.RIGHT:
-                if self._velRand_x < 0:
-                    self._velRand_x = -self._velRand_x
+                if self._vel_x < 0:
+                    self._vel_x = -self._vel_x
 
             if symbol == pg.window.key.DOWN:
-                if self._velRand_y > 0:
-                    self._velRand_y = -self._velRand_y
+                if self._vel_y > 0:
+                    self._vel_y = -self._vel_y
+
+            if symbol == pg.window.key.NUM_0:
+                self._vel_x = 0
+                self._vel_y = 0
 
     def on_resize(self, width, height):
         super().on_resize(width, height)
